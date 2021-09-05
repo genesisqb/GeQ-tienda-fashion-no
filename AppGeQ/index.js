c2e8a3e65c51d19218441e5d1441feca6af72c9b
genesisqb <86036219+genesisqb@users.noreply.github.com>
2021-09-05 18:21:34 -0300

    Delete AppGeQ directory

--- AppGeQ/index.js
+++
@@ -1,8 +0,0 @@
-function menu() {
-    const myMenu = document.getElementById("myLinks");
-    if (myMenu.style.display === "block") {
-        myMenu.style.display = "none";
-    } else {
-        myMenu.style.display = "block";
-    }
-}
